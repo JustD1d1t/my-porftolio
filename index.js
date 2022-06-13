80182bbf5a472d4c840883c99c4dc3a82be014e8
JustD1d1t <starzyk.dawid.0826@gmail.com>
2022-06-13 19:38:50 +0200

    hamburger + new project

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-import "./nav.js";
+const handleHamburger = () => {
+  console.log("test");
+  const hamburger = document.querySelector(".navbar__hamburger");
+  const mobileMenu = document.querySelector(".navbar__links");
+  const links = document.querySelectorAll(".navbar__link");
+  function toggleMenu() {
+    mobileMenu.classList.toggle("active");
+    hamburger.classList.toggle("is-active");
+  }
+  hamburger.addEventListener("click", toggleMenu);
+  if (window.innerWidth < 1440) {
+    links.forEach((link) => {
+      link.addEventListener("click", toggleMenu);
+    });
+  }
+};
+
+handleHamburger();
